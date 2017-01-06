b488d58dddd3d61490bfd67362d162c6369a14aa
Maxwell Cady <maxwell.josef@gmail.com>
2017-01-06 13:40:07 -0800

    finish functionality of search by symptom or specialty. style page

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,17 +1,55 @@
 var apiKey = require('./../.env').apiKey;
-var getDoctors = require('./../js/doctor.js').getDoctors;
+var issueSearch = require('./../js/doctor.js').issueSearch;
+var getSpecialties = require('./../js/doctor.js').getSpecialties;
+var specialtySearch = require('./../js/doctor.js').specialtySearch;
 
-var displayDoctors = function(medicalIssue, doctorsList) {
-  console.log(doctorsList);
+
+var displayDoctors = function(searchType, doctorsList) {
+  $('#doctors').empty();
+  if (doctorsList.length === 0) {
+    $('#doctors').append("Sorry, no doctors meet your search criteria. Please try a different symptom or specialty.");
+  }
   for(var i = 0; i<doctorsList.length; i++) {
-    $('#doctors').append(doctorsList[i].profile.first_name);
+    $('#doctors').append("<div class='doctor'></div>");
+    $('.doctor:nth-child(' + (i+1) + ')').append(
+      "<div class='doctor-title'>" +
+      doctorsList[i].profile.first_name +
+      " " + doctorsList[i].profile.last_name +
+      ", " + doctorsList[i].profile.title +
+       "</div>" +
+       "<div class='doctor-practice'>" +
+       doctorsList[i].practices[0].name +
+        "</div>" +
+       "<img class='doctor-image' src='" +
+       doctorsList[i].profile.image_url + "' alt='A picture of Dr.'" + doctorsList[i].profile.first_name + ">" +
+       "<div class='doctor-bio'>" +
+       doctorsList[i].profile.bio.slice(0, 500) + "...</div>"
+    );
+  }
+};
+
+var displaySpecialties = function(specialtiesList) {
+  console.log(specialtiesList);
+  for (var i = 0; i<specialtiesList.length; i++) {
+    $('#specialties').append(
+      "<option value='" + specialtiesList[i].uid + "'>" + specialtiesList[i].name + "</option>"
+    )
   }
 };
 
 $(document).ready(function() {
-  $('#getDoctors').submit(function(event) {
+  getSpecialties(displaySpecialties);
+  $('#issueSearch').submit(function(event) {
     event.preventDefault();
     var medicalIssue = $('#issue').val();
-    getDoctors(medicalIssue, displayDoctors);
+    issueSearch(medicalIssue, displayDoctors);
   });
+
+  $('#specialtiesSearch').submit(function(event) {
+    event.preventDefault();
+    var specialty = $('#specialties').val();
+    specialtySearch(specialty, displayDoctors);
+  });
+
+
 });
--- js/doctor.js
+++ js/doctor.js
@@ -1,13 +1,43 @@
 var apiKey = require('./../.env').apiKey;
 
-exports.getDoctors = function(medicalIssue, displayFunction) {
-  var doctorsList = [];
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+exports.getSpecialties = function(displayFunction) {
+  var specialtiesList = [];
+  $.get('https://api.betterdoctor.com/2016-03-01/specialties?user_key=' + apiKey)
    .then(function(result) {
+     for (var i = 0; i<result.data.length; i++) {
+       specialtiesList.push(result.data[i]);
+     }
+     displayFunction(specialtiesList);
+    })
+   .fail(function(error){
+      console.log("request failed");
+    });
+};
+
+exports.issueSearch = function(medicalIssue, displayFunction) {
+  var doctorsList = [];
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C&user_location=45.5231%2C-122.6765&skip=0&limit=50&user_key=' + apiKey)
+   .then(function(result) {
+     console.log(result);
       for (var i = 0; i<result.data.length; i++) {
         doctorsList.push(result.data[i]);
       }
       displayFunction(medicalIssue, doctorsList);
+    })
+   .fail(function(error){
+      console.log("request failed");
+    });
+};
+
+exports.specialtySearch = function(specialty, displayFunction) {
+  var doctorsList = [];
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?specialty_uid=' + specialty + '&location=45.5231%2C-122.6765%2C&user_location=45.5231%2C-122.6765&skip=0&limit=50&user_key=' + apiKey)
+   .then(function(result) {
+     console.log(result);
+      for (var i = 0; i<result.data.length; i++) {
+        doctorsList.push(result.data[i]);
+      }
+      displayFunction(specialty, doctorsList);
     })
    .fail(function(error){
       console.log("request failed");
